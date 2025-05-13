8570feb095b3ade0f914390bbd48249b2fe7074b
Arthur <60153157097@senacrs.edu.br>
2025-05-13 16:22:24 -0300

    front-end v0.1

---
+++ lobby/lobby.js
@@ -0,0 +1,11 @@
+const register = document.querySelector(".register");
+
+register.addEventListener("click", () => {
+    window.location.href = "../register/register.html";
+});
+
+const registre_se = document.querySelector(".registre-se");
+
+registre_se.addEventListener("click", () => {
+    window.location.href = "../registre-se/registre-se.html";
+});
