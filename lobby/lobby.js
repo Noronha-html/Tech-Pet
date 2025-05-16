a4f7cf2c20056a03ec07406fd2b66c993f4e4b0f
Arthur <60153157097@senacrs.edu.br>
2025-05-16 11:41:26 -0300

    v0.1.16

--- lobby/lobby.js
+++ lobby/lobby.js
@@ -9,3 +9,9 @@ const registre_se = document.querySelector(".registre-se");
 registre_se.addEventListener("click", () => {
     window.location.href = "../registre-se/registre-se.html";
 });
+
+const achei = document.querySelector(".achei");
+
+achei.addEventListener("click", () => {
+    window.location.href = "../achei-um-pet/achei-um-pet.html";
+});
