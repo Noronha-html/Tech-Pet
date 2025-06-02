141910611f1c7bf8680e353553fa40481da0b59a
Felipe Noronha <86336436004@senacrs.edu.br>
2025-06-02 10:21:07 -0300

    v0.6.2

--- registre-se/registre-se_processo.php
+++ registre-se/registre-se_processo.php
@@ -24,7 +24,34 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     } else {
         if (!validarNome()) {
             exit("Nome inválido. Por favor, use apenas letras e espaços.");
-        } else {
+        }
+
+        if (!validarSenha()) {
+            exit("Senha inválida. Por favor, use apenas letras e espaços.");
+        }
+
+        if(!confirmarSenha()) {
+            exit("As senhas não coincidem. Por favor, tente novamente.");
+        }
+
+        if(!validarEstadoCidade()) {
+            exit("Por favor, selecione um estado e uma cidade.");
+        }
+
+        if(!validarEmail()) {
+            exit("E-mail inválido. Por favor, insira um e-mail válido.");
+        }
+
+        if(!validarDataNascimento()) {
+            exit("Data de nascimento inválida. Por favor, insira uma data válida.");
+        }
+
+        if(!validarTelefone()) {
+            exit("Número de telefone inválido. Por favor, insira um número válido.");
+        }
+
+        if(validarNome() && validarSenha() && confirmarSenha() && validarEstadoCidade() && validarEmail() && validarDataNascimento() && validarTelefone()) {
+            // Insere os dados no banco de dados
             $insert1 = "INSERT INTO `pessoas`(`Nome`, `Estado`, `Cidade`, `Email`, `DataNascimento`, `Whatsapp`, `Senha`, `Excluido`) VALUES ('$name','$state','$city','$email','$birthDate','$whatsapp','$password',0)";  
             $result = mysqli_query($conn, $insert1);
 
@@ -46,6 +73,65 @@ function validarNome() {
         if (preg_match('/^[a-zA-Z\s]+$/', $name)) {
             return true;
         }
+    }
+    return false;
+}
+
+function validarSenha() {
+    if (isset($_POST['senhaRegistro']) && !empty($_POST['senhaRegistro'])) {
+        $password = $_POST['senhaRegistro'];
+        return true;
+    }
+    return false;
+}
+
+function confirmarSenha() {
+    if (isset($_POST['confirmarSenhaRegistro']) && !empty($_POST['confirmarSenhaRegistro'])) {
+        $confirmPassword = $_POST['confirmarSenhaRegistro'];
+        if(isset($_POST['senhaRegistro']) && $_POST['senhaRegistro'] === $confirmPassword) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function validarEstadoCidade() {
+    if (isset($_POST['estados']) && !empty($_POST['estados']) && isset($_POST['cidades']) && !empty($_POST['cidades'])) {
+        $state = $_POST['estados'];
+        $city = $_POST['cidades'];
+        return true;
+    }
+    return false;
+}
+
+function validarEmail() {
+    if (isset($_POST['email']) && !empty($_POST['email'])) {
+        $email = $_POST['email'];
+        if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function validarDataNascimento() {
+    if (isset($_POST['dtnasc']) && !empty($_POST['dtnasc'])) {
+        $birthDate = $_POST['dtnasc'];
+        $date = DateTime::createFromFormat('Y-m-d', $birthDate);
+        if ($date && $date->format('Y-m-d') === $birthDate) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function validarTelefone() {
+    if (isset($_POST['wpp']) && !empty($_POST['wpp'])) {
+        $whatsapp = $_POST['wpp'];
+        // Verifica se o número de telefone contém apenas dígitos e tem entre 10 e 11 dígitos
+        //if (preg_match('/^\d{10,11}$/', $whatsapp)) {
+            return true;
+        //}
     }
     return false;
 }
