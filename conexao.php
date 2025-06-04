5192719a4e03bc6fa359a432cea268441e943b34
Arthur <60153157097@senacrs.edu.br>
2025-06-04 20:11:46 -0300

    v0.7.3

--- conexao.php
+++ conexao.php
@@ -1,13 +1,14 @@
 <?php
 
-    $host = "localhost";
-    $user = "root";
-    $password = "";
-    $db = "techpet";
- 
-    $conn = new mysqli($host, $user, $password, $db);
- 
-    if(!($conn)){
-        echo "Conexão não estabelecida";
-    }
+$host = "mysql.techpet.app.br";
+$user = "techpet";
+$password = "aN3L8TE88dyJ";
+$db = "techpet";
+
+$conn = new mysqli($host, $user, $password, $db);
+
+// Verificar se houve erro
+if ($conn->connect_error) {
+    die("Erro na conexão com o banco de dados: " . $conn->connect_error);
+}
 ?>
