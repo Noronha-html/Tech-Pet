0664b27b9bc7a3bc9a9118f221d712938ed88f6c
Felipe Noronha <felipe.dariva07@gmail.com>
2025-06-06 00:18:39 -0300

    v0.8.5

--- achei-um-pet/achei-um-pet.js
+++ achei-um-pet/achei-um-pet.js
@@ -1,6 +1,6 @@
-let inputNumero = document.getElementById('numeroSerie');
+//let inputNumero = document.getElementById('numeroSerie');
 
-inputNumero.addEventListener('input', function() {
+/*inputNumero.addEventListener('input', function() {
     // Remove tudo que não for número e limita a 3 dígitos
     let valor = inputNumero.value.replace(/[^0-9]/g, '').slice(0, 3);
     if (valor.length > 0) {
@@ -8,16 +8,37 @@ inputNumero.addEventListener('input', function() {
     } else {
         inputNumero.value = '';
     }
-});
+});*/
+const numeroSerie = document.getElementById("numeroSerie");
+
+function validarnumeroSerie() {
+    //numeroSerie.addEventListener('input', function() {
+        // Remove tudo que não for número e limita a 3 dígitos
+        let valor = numeroSerie.value.replace(/[^0-9]/g, '').slice(0, 3);
+        if (valor.length > 0) {
+            numeroSerie.value = '#' + valor;
+        } else {
+            numeroSerie.value = '';
+        }
+    //});
+
+    if(numeroSerie.value === "") {
+        return false;
+    }
+
+    return true;
+}
 
 // Garante que o usuário não possa digitar antes da hashtag ou removê-la se houver número
 inputNumero.addEventListener('keydown', function(e) {
-    // Se já existe número, impede digitar antes da hashtag
-    if (inputNumero.value.startsWith('#') && inputNumero.selectionStart === 0 && e.key.length === 1 && /[0-9]/.test(e.key)) {
-        e.preventDefault();
-    }
-    // Impede remover a hashtag se houver número
-    if (inputNumero.value.startsWith('#') && inputNumero.selectionStart === 1 && (e.key === 'Backspace' || e.key === 'Delete') && inputNumero.value.length > 1) {
-        e.preventDefault();
+    if(validarnumeroSerie()) {
+        // Se já existe número, impede digitar antes da hashtag
+        if (inputNumero.value.startsWith('#') && inputNumero.selectionStart === 0 && e.key.length === 1 && /[0-9]/.test(e.key)) {
+            e.preventDefault();
+        }
+        // Impede remover a hashtag se houver número
+        if (inputNumero.value.startsWith('#') && inputNumero.selectionStart === 1 && (e.key === 'Backspace' || e.key === 'Delete') && inputNumero.value.length > 1) {
+            e.preventDefault();
+        }
     }
 });
